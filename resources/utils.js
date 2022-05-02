78c0511fbf196c479b9e8af8864701835539bc39
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-05-02 22:42:45 +0900

    실제 고양이 데이터 받아와서 연동

--- resources/utils.js
+++ resources/utils.js
@@ -19,6 +19,16 @@ const jsonLocalStorage = {
 const fetchCat = async (text) => {
   const OPEN_API_DOMAIN = "https://cataas.com";
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
-  const responseJson = await response.json();
-  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+  const responseJson = await response.json(); // 응답을 json파일로 변환
+  return `${OPEN_API_DOMAIN}/${responseJson.url}`; // img url을 반환
 };
+
+const fetchCat = () => {
+  const OPEN_API_DOMAIN = "https://cataas.com";
+ fetch(`${OPEN_API_DOMAIN}/cat/says/hello?json=true`).then(
+   (response) => {response.json(); console.log(response);}
+ ).then((responseJson) => {
+  console.log(responseJson);
+  // return `${OPEN_API_DOMAIN}/${responseJson.url}`
+ } 
+ )};
